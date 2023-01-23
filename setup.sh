23ceef8117164b86bc2705e9fd6316d7c79724d9
Sam Burkhard <samryanburk@icloud.com>
2023-01-23 10:04:12 -0500

    Add brew to shell path

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ xcode-select --install
 if test ! $(which brew); then
   echo "🍺 Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)" 
 fi
 
 echo "✨ Installing Homebrew Software..."
